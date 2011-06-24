6ae0f964f338b48e82352f7c7b64a00e034f0c02
Paul Crowley <paul@lshift.net>
2011-06-24 11:23:05 +0100

    Add logging to InferenceEngine
    --HG--
    branch : bug12

--- src/main/java/net/lshift/spki/suiteb/InferenceEngine.java
+++ src/main/java/net/lshift/spki/suiteb/InferenceEngine.java
@@ -7,10 +7,15 @@ import java.util.List;
 import java.util.Map;
 
 import net.lshift.spki.InvalidInputException;
+import net.lshift.spki.convert.ConvertUtils;
 import net.lshift.spki.suiteb.sexpstructs.EcdhItem;
 import net.lshift.spki.suiteb.sexpstructs.Sequence;
 import net.lshift.spki.suiteb.sexpstructs.SequenceItem;
 import net.lshift.spki.suiteb.sexpstructs.SimpleMessage;
+
+import org.apache.commons.codec.binary.Base64;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Take a bunch of SequenceItems and figure out what you can infer from them.
@@ -20,6 +25,9 @@ import net.lshift.spki.suiteb.sexpstructs.SimpleMessage;
  * fixed and it will do for now.
  */
 public class InferenceEngine {
+    private static final Logger LOG
+        = LoggerFactory.getLogger(InferenceEngine.class);
+
     private final Map<DigestSha384, PrivateEncryptionKey> dhKeys
         = new HashMap<DigestSha384, PrivateEncryptionKey>();
     private final Map<DigestSha384, PublicSigningKey> dsaKeys
@@ -35,6 +43,22 @@ public class InferenceEngine {
     private final List<SimpleMessage> messages
         = new ArrayList<SimpleMessage>();
 
+    private final Map<String, String> byteNames = new HashMap<String,String>();
+
+    private String bytesString(final byte[] bytes) {
+        final String string = "|" + Base64.encodeBase64String(bytes) + "|";
+        String name = byteNames.get(string);
+        if (name == null) {
+            name = Integer.toString(byteNames.size(), 36);
+            byteNames.put(string, name);
+        }
+        return "\"" + name + "\"-" + string;
+    }
+
+    private String digestString(DigestSha384 digest) {
+        return bytesString(digest.getBytes());
+    }
+
     public void process(final SequenceItem item) throws InvalidInputException {
         process(item, null);
     }
@@ -45,6 +69,12 @@ public class InferenceEngine {
         if (signer == null) {
             final DigestSha384 digest = DigestSha384.digest(item);
             signer = signedBy.get(digest);
+            if (signer != null) {
+                LOG.debug("Signed object found, signer {} signed {}",
+                    digestString(signer), digestString(digest));
+                LOG.debug(
+                    ConvertUtils.prettyPrint(SequenceItem.class, item));
+            }
         }
         if (item instanceof Sequence) {
             process((Sequence) item, signer);
@@ -71,44 +101,55 @@ public class InferenceEngine {
     }
 
     public void process(final PrivateEncryptionKey privateKey) {
-        dhKeys.put(privateKey.getPublicKey().getKeyId(), privateKey);
+        final DigestSha384 keyId = privateKey.getPublicKey().getKeyId();
+        LOG.debug("Adding private encryption key: {}", digestString(keyId));
+        dhKeys.put(keyId, privateKey);
     }
 
     public void process(final Sequence items, final DigestSha384 signer)
         throws InvalidInputException {
+        LOG.debug("Processing sequence...");
         for (final SequenceItem item: items.sequence) {
             process(item, signer);
         }
+        LOG.debug("...sequence processed.");
     }
 
     public void process(final EcdhItem item) {
         final PrivateEncryptionKey key = dhKeys.get(item.recipient);
-        if (key != null) {
+        if (key == null) {
+            LOG.debug("Skipping encrypted packet for recipient {}",
+                digestString(item.recipient));
+        } else {
+            LOG.debug("Processing encrypted packet for recipient {}",
+                digestString(item.recipient));
             process(new AesKey(key.getKey(item.ephemeralKey)));
         }
     }
 
     public void process(final AesKey key) {
-//        try {
-//            System.out.println("Processing key:");
-//            ConvertUtils.prettyPrint(AesKey.class, key, System.out);
-//            System.out.println("ID:");
-//            ConvertUtils.prettyPrint(AesKeyId.class, key.getKeyId(), System.out);
-//        } catch (IOException e) {
-//            throw new RuntimeException(e);
-//        }
+        LOG.debug("Adding AES key id {}",
+            bytesString(key.getKeyId().keyId));
         aesKeys.put(key.getKeyId(), key);
     }
 
     public void process(final PublicSigningKey pKey) {
-        dsaKeys.put(pKey.getKeyId(), pKey);
+        final DigestSha384 keyId = pKey.getKeyId();
+        LOG.debug("Adding public signing key: {}", digestString(keyId));
+        dsaKeys.put(keyId, pKey);
     }
 
     public void process(final Signature sig) throws InvalidInputException {
         final PublicSigningKey pKey = dsaKeys.get(sig.keyId);
-        if (pKey == null) return;
+        if (pKey == null) {
+            LOG.debug("Skipping signature from unknown signer {} for {}",
+                digestString(sig.keyId), digestString(sig.digest));
+            return;
+        }
         if (!pKey.validate(sig.digest, sig.rawSignature))
             throw new InvalidInputException("Sig validation failure");
+        LOG.debug("Signer {} attests to {}",
+            digestString(sig.keyId), digestString(sig.digest));
         // FIXME: assert that it's not already signed?
         signedBy.put(sig.digest, sig.keyId);
     }
@@ -116,33 +157,34 @@ public class InferenceEngine {
     public void process(final SimpleMessage message, final DigestSha384 signer) {
         messages.add(message);
         if (signer != null) {
+            LOG.debug("Found message signed by {}:\n{}",
+                digestString(signer),
+                ConvertUtils.prettyPrint(SimpleMessage.class, message));
             listPut(hasSigned, signer, message);
+        } else {
+            LOG.debug("Message has no known signer:\n{}",
+                ConvertUtils.prettyPrint(SimpleMessage.class, message));
         }
     }
 
     public void process(final AesPacket packet) throws InvalidInputException {
-//        try {
-//            System.out.println("Packet encrypted with:");
-//            ConvertUtils.prettyPrint(AesKeyId.class, packet.keyId, System.out);
-//        } catch (IOException e) {
-//            throw new RuntimeException(e);
-//        }
         final AesKey key = aesKeys.get(packet.keyId);
         if (key != null) {
             final SequenceItem contents = key.decrypt(packet);
-//            try {
-//                System.out.println("Contents:");
-//                ConvertUtils.prettyPrint(SequenceItem.class, contents,
-//                    System.out);
-//            } catch (IOException e) {
-//                throw new RuntimeException(e);
-//            }
+            LOG.debug("Decrypted packet with key {}:\n{}",
+                bytesString(packet.keyId.keyId),
+                ConvertUtils.prettyPrint(SequenceItem.class, contents));
             process(contents);
+        } else {
+            LOG.debug("Skipping packet encrypted with unknown key {}",
+                bytesString(packet.keyId.keyId));
         }
     }
 
     public void process(final DigestSha384 digest, final DigestSha384 signer) {
         if (signer != null) {
+            LOG.debug("Chained signature from {} for {}",
+                digestString(signer), digestString(digest));
             signedBy.put(digest, signer);
         }
     }
