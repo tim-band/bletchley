d944538a20c2049147e9234a77c3ebd66e12bb1d
Paul Crowley <paul@lshift.net>
2011-11-10 16:47:52 +0000

    Cosmetic changes, change branch name
    --HG--
    branch : refactor

--- src/main/java/net/lshift/spki/convert/ByteArrayStepConverter.java
+++ src/main/java/net/lshift/spki/convert/ByteArrayStepConverter.java
@@ -1,18 +1,14 @@
 package net.lshift.spki.convert;
 
-
+/**
+ * Convenient superclass for converters that go via byte arrays.
+ */
 public abstract class ByteArrayStepConverter<T>
     extends StepConverter<T, byte[]> {
 
-    public ByteArrayStepConverter() {
-        super();
-    }
+    public ByteArrayStepConverter() { super(); }
 
-    @Override
-    public String getName() { return null; }
+    @Override public String getName() { return null; }
 
-    @Override
-    protected Class<byte[]> getStepClass() {
-        return byte[].class;
-    }
+    @Override protected Class<byte[]> getStepClass() { return byte[].class; }
 }
