a4115b076e83d527c5f41b13d28b6cbf9ad7edc4
Paul Crowley <paul@lshift.net>
2011-11-11 18:46:35 +0000

    Oops, it would have overwritten the bytes in a digest! Also simplify and comment.
    --HG--
    branch : diaas-bug209

--- src/main/java/net/lshift/spki/suiteb/fingerprint/FingerprintUtils.java
+++ src/main/java/net/lshift/spki/suiteb/fingerprint/FingerprintUtils.java
@@ -1,7 +1,6 @@
 package net.lshift.spki.suiteb.fingerprint;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
 import net.lshift.spki.suiteb.DigestSha384;
@@ -10,11 +9,14 @@ import org.apache.commons.io.IOUtils;
 import org.bouncycastle.crypto.Digest;
 import org.bouncycastle.crypto.digests.SHA384Digest;
 
+/**
+ * Generate a fingerprint from a digest using a word list.
+ * The fingerprints are roughly fairly chosen from 2^192.5
+ * possibilities.
+ */
 public class FingerprintUtils {
-    public static final int GROUP_LENGTH = 5;
     public static final String SEPARATORS = "--/--/--/--/--";
     public static final int NUM_GROUPS = SEPARATORS.length()+1;
-    public static final int NUM_CHARS = GROUP_LENGTH * NUM_GROUPS;
 
     public static final List<String> WORDLIST;
     static {
@@ -27,56 +29,44 @@ public class FingerprintUtils {
     }
     public static final int NUM_WORDS = WORDLIST.size();
 
-    public static <T> String getFingerprint(
-        Class<T> clazz,
-        T o) {
+    public static <T> String getFingerprint(Class<T> clazz, T o) {
         return getFingerprint(DigestSha384.digest(clazz, o));
     }
 
     public static String getFingerprint(DigestSha384 digest) {
-        List<String> groups = getFingerprintGroups(digest.getBytes());
-        StringBuffer res = new StringBuffer(NUM_CHARS + NUM_GROUPS -1);
-        for (int i = 0; i < NUM_GROUPS; i++) {
-            if (i > 0) {
-                res.append(SEPARATORS.charAt(i-1));
-            }
-            res.append(groups.get(i));
-        }
-        return res.toString();
+        return getFingerprint(digest.getBytes());
     }
 
-    private static List<String> getFingerprintGroups(byte[] bs) {
+    private static String getFingerprint(byte[] bs) {
         byte[] bytes = bs;
-        List<String> res = new ArrayList<String>();
+        StringBuffer res = new StringBuffer();
+        int i = 0;
         int x = 0;
         int xlim = 1;
+        // Invariant: x is a random number 0 <= x < xlim
         while (true) {
             for (byte b: bytes) {
-                x *= 256;
-                xlim *= 256;
-                x += (b & 0xff);
+                x *= 256; x += (b & 0xff); xlim *= 256;
                 if (xlim >= NUM_WORDS) {
                     int k = xlim / NUM_WORDS;
                     if (x < k * NUM_WORDS) {
-                        res.add(WORDLIST.get(x % NUM_WORDS));
-                        if (res.size() == NUM_GROUPS) {
-                            return res;
+                        res.append(WORDLIST.get(x % NUM_WORDS));
+                        if (i == SEPARATORS.length()) {
+                            return res.toString();
                         }
-                        x /= NUM_WORDS;
-                        xlim = k;
+                        res.append(SEPARATORS.charAt(i));
+                        i ++;
+                        x /= NUM_WORDS; xlim = k;
                     } else {
-                        x -= k * NUM_WORDS;
-                        xlim -= k * NUM_WORDS;
+                        x -= k * NUM_WORDS; xlim -= k * NUM_WORDS;
                     }
                 }
             }
-            redigest(bytes);
+            Digest digest = new SHA384Digest();
+            digest.update(bytes, 0, bytes.length);
+            if (bytes == bs)
+                bytes = new byte[bs.length];
+            digest.doFinal(bytes, 0);
         }
-    }
-
-    private static void redigest(byte[] bytes) {
-        Digest digest = new SHA384Digest();
-        digest.update(bytes, 0, bytes.length);
-        digest.doFinal(bytes, 0);
     }
 }
