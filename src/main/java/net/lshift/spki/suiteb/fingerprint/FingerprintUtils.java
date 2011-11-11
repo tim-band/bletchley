73acae43fdbf0c7ee604d971a4c891957d6e17b4
Paul Crowley <paul@lshift.net>
2011-11-11 16:55:25 +0000

    Generate a 192-bit strength fingerprint and use a wordlist for convenience.
    --HG--
    branch : diaas-bug209

--- src/main/java/net/lshift/spki/suiteb/fingerprint/FingerprintUtils.java
+++ src/main/java/net/lshift/spki/suiteb/fingerprint/FingerprintUtils.java
@@ -1,16 +1,31 @@
 package net.lshift.spki.suiteb.fingerprint;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 import net.lshift.spki.suiteb.DigestSha384;
 
+import org.apache.commons.io.IOUtils;
 import org.bouncycastle.crypto.Digest;
 import org.bouncycastle.crypto.digests.SHA384Digest;
 
 public class FingerprintUtils {
-    private static final int STRETCH_ROUNDS = 1<<8;
     public static final int GROUP_LENGTH = 5;
-    public static final String SEPARATORS = "--/--/--";
+    public static final String SEPARATORS = "--/--/--/--/--";
     public static final int NUM_GROUPS = SEPARATORS.length()+1;
     public static final int NUM_CHARS = GROUP_LENGTH * NUM_GROUPS;
+
+    public static final List<String> WORDLIST;
+    static {
+        try {
+            WORDLIST = IOUtils.readLines(
+                FingerprintUtils.class.getResourceAsStream("wordlist"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public static final int NUM_WORDS = WORDLIST.size();
 
     public static <T> String getFingerprint(
         Class<T> clazz,
@@ -19,30 +34,39 @@ public class FingerprintUtils {
     }
 
     public static String getFingerprint(DigestSha384 digest) {
-        String ungrouped = getUngroupedFingerprint(digest.getBytes());
+        List<String> groups = getFingerprintGroups(digest.getBytes());
         StringBuffer res = new StringBuffer(NUM_CHARS + NUM_GROUPS -1);
         for (int i = 0; i < NUM_GROUPS; i++) {
             if (i > 0) {
                 res.append(SEPARATORS.charAt(i-1));
             }
-            res.append(ungrouped.substring(i*GROUP_LENGTH, (i+1)*GROUP_LENGTH));
+            res.append(groups.get(i));
         }
         return res.toString();
     }
 
-    private static String getUngroupedFingerprint(byte[] bs) {
+    private static List<String> getFingerprintGroups(byte[] bs) {
         byte[] bytes = bs;
-        for (int i = 0; i < STRETCH_ROUNDS; i++) {
-            redigest(bytes);
-        }
-        StringBuffer res = new StringBuffer(NUM_CHARS);
+        List<String> res = new ArrayList<String>();
+        int x = 0;
+        int xlim = 1;
         while (true) {
             for (byte b: bytes) {
-                int ival = b & 0xff;
-                if (ival < 26*9) {
-                    res.append((char)('a' +(ival % 26)));
-                    if (res.length() == NUM_CHARS) {
-                        return res.toString();
+                x *= 256;
+                xlim *= 256;
+                x += (b & 0xff);
+                if (xlim >= NUM_WORDS) {
+                    int k = xlim / NUM_WORDS;
+                    if (x < k * NUM_WORDS) {
+                        res.add(WORDLIST.get(x % NUM_WORDS));
+                        if (res.size() == NUM_GROUPS) {
+                            return res;
+                        }
+                        x /= NUM_WORDS;
+                        xlim = k;
+                    } else {
+                        x -= k * NUM_WORDS;
+                        xlim -= k * NUM_WORDS;
                     }
                 }
             }
--- src/test/java/net/lshift/spki/suiteb/fingerprint/FingerprintTest.java
+++ src/test/java/net/lshift/spki/suiteb/fingerprint/FingerprintTest.java
@@ -1,22 +1,55 @@
 package net.lshift.spki.suiteb.fingerprint;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import net.lshift.spki.sexpform.Create;
+import static net.lshift.spki.sexpform.Create.atom;
+import static net.lshift.spki.sexpform.Create.list;
+import static org.hamcrest.CoreMatchers.is;
 import net.lshift.spki.sexpform.Sexp;
 
-import org.junit.Test;
+import org.junit.Assert;
+import org.junit.experimental.theories.DataPoints;
+import org.junit.experimental.theories.Theories;
+import org.junit.experimental.theories.Theory;
+import org.junit.runner.RunWith;
 
+@RunWith(Theories.class)
 public class FingerprintTest {
-    // FIXME ideas for more meaningful tests welcome
-    @Test
-    public void canGenerateFingerprint() {
-        Sexp testSexp = Create.list("fooble");
-        String fingerprint
-            = FingerprintUtils.getFingerprint(Sexp.class, testSexp);
-        assertNotNull(fingerprint);
-        System.out.println(fingerprint);
-        assertEquals(fingerprint,
-            FingerprintUtils.getFingerprint(Sexp.class, testSexp));
+    private static class TestPair {
+        private final Sexp test;
+        private final String fingerprint;
+
+        public TestPair(Sexp test, String fingerprint) {
+            super();
+            this.test = test;
+            this.fingerprint = fingerprint;
+        }
+
+        public Sexp getTest() {
+            return test;
+        }
+
+        public String getFingerprint() {
+            return fingerprint;
+        }
+    }
+
+    @DataPoints
+    public static TestPair[] data() {
+        return new TestPair[] {
+            new TestPair(atom(""),
+                "yet-heaps-geo/owner-jp-siege/serve-jesus-gordon/emma-maybe-havoc/oaf-glory-orbs"),
+            new TestPair(atom("hello"),
+                "sutton-awful-sperm/welch-reek-tyke/hhhh-nag-brady/dn-fatal-mn/jive-text-jp"),
+            new TestPair(list("hello"),
+                "says-habit-morsel/lice-mouth-take/vomit-waste-sleds/fled-skunk-wino/gu-twin-cock"),
+            new TestPair(list("hello", atom("there")),
+                "bow-owl-dues/peak-check-eli/bang-posse-fogy/jewel-ages-sum/clumsy-flirt-like"),
+        };
+    }
+
+    @Theory
+    public void theoryFingerprintsAreStable(TestPair pair) {
+        Assert.assertThat(
+            FingerprintUtils.getFingerprint(Sexp.class, pair.getTest()),
+            is(pair.getFingerprint()));
     }
 }
