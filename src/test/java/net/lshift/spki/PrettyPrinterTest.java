e9ab2b4ede8de1d2450e58b1a0d9f7648a02b5ce
Paul Crowley <paul@lshift.net>
2011-12-05 15:46:58 +0000

    Cleanup of pretty printer unit test (and new test)

--- src/test/java/net/lshift/spki/PrettyPrinterTest.java
+++ src/test/java/net/lshift/spki/PrettyPrinterTest.java
@@ -3,8 +3,10 @@ package net.lshift.spki;
 import static net.lshift.spki.sexpform.Create.atom;
 import static net.lshift.spki.sexpform.Create.list;
 import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertThat;
 
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -12,6 +14,7 @@ import net.lshift.spki.convert.ConvertUtils;
 import net.lshift.spki.sexpform.Sexp;
 
 import org.apache.commons.io.IOUtils;
+import org.junit.Test;
 import org.junit.experimental.theories.DataPoints;
 import org.junit.experimental.theories.Theories;
 import org.junit.experimental.theories.Theory;
@@ -19,50 +22,68 @@ import org.junit.runner.RunWith;
 
 @RunWith(Theories.class)
 public class PrettyPrinterTest {
+    private static final boolean WRITE_TEST_OUTPUT = false;
     private static class TestPair {
+        private final String resourceName;
         private final Sexp sexp;
-        //private final String resourceName;
-        private final String prettyPrinted;
 
-        public TestPair(Sexp sexp, String resourceName) throws IOException {
+        public TestPair(String resourceName, Sexp sexp) {
             super();
+            this.resourceName = resourceName;
             this.sexp = sexp;
-            //this.resourceName = resourceName;
-            final InputStream resourceAsStream
-                = PrettyPrinterTest.class.getResourceAsStream(
-                    "_PrettyPrinterTest/" + resourceName);
-            this.prettyPrinted = resourceAsStream == null ?  null :
-                IOUtils.toString(resourceAsStream);
+        }
+
+        public TestPair(int i, Sexp sexp) {
+            this(Integer.toString(i) + ".spki", sexp);
         }
 
         public Sexp getSexp() { return sexp; }
 
-        //public String getResourceName() { return resourceName; }
+        public String getResourceName() { return resourceName; }
 
-        public String getPrettyPrinted() { return prettyPrinted; }
+        public String getPrettyPrinted()
+            throws IOException {
+            final InputStream resourceAsStream
+                = PrettyPrinterTest.class.getResourceAsStream(
+                    "_PrettyPrinterTest/" + getResourceName());
+            return resourceAsStream == null ? null : IOUtils
+                            .toString(resourceAsStream);
+        }
     }
 
     @DataPoints
-    public static TestPair[] data() throws IOException {
+    public static TestPair[] data() {
+        int counter = 1;
         return new TestPair[] {
-            new TestPair(atom("foo"), "1"),
-            new TestPair(list("foo"), "2"),
-            new TestPair(list("foo", list("bar", atom("baz")), atom("foof")), "3"),
-            new TestPair(list("foo",  atom("baz")), "4"),
-            new TestPair(list("fo\"o",  atom("baz")), "5"),
-            new TestPair(list("fo-o",  atom("baz")), "6"),
-            new TestPair(list("foo bar",  atom("baz")), "7"),
+            new TestPair(counter++, atom("foo")),
+            new TestPair(counter++, list("foo")),
+            new TestPair(counter++, list("foo", list("bar", atom("baz")), atom("foof"))),
+            new TestPair(counter++, list("foo",  atom("baz"))),
+            new TestPair(counter++, list("fo\"o",  atom("baz"))),
+            new TestPair(counter++, list("fo-o",  atom("baz"))),
+            new TestPair(counter++, list("foo bar",  atom("baz"))),
+            new TestPair(counter++, list("\0x80fsssssssssssssoo bar",  atom("baz"))),
         };
     }
 
     @Theory
-    public void theoryPrettyPrintingIsStable(TestPair pair) {
+    public void theoryPrettyPrintingIsStable(TestPair pair) throws IOException {
         final String prettyPrinted = ConvertUtils.prettyPrint(Sexp.class, pair.getSexp());
-//        FileOutputStream out = new FileOutputStream("/tmp/out/" + pair.getResourceName());
-//        IOUtils.write(prettyPrinted, out);
-//        out.close();
-        assertThat(
-            prettyPrinted,
-            is(pair.getPrettyPrinted()));
+        assertThat(prettyPrinted, is(pair.getPrettyPrinted()));
+    }
+
+    @Test
+    public void handleWriteTestOutput() throws IOException {
+        if (WRITE_TEST_OUTPUT) {
+            for (TestPair pair : data()) {
+                final String prettyPrinted = ConvertUtils.prettyPrint(
+                    Sexp.class, pair.getSexp());
+                FileOutputStream out
+                    = new FileOutputStream("/tmp/out/" + pair.getResourceName());
+                IOUtils.write(prettyPrinted, out);
+                out.close();
+            }
+        }
+        assertFalse(WRITE_TEST_OUTPUT);
     }
 }
