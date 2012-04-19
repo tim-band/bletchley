c6b7820c2190655f44d50209067bfffacce1b8bb
Paul Crowley <paul@lshift.net>
2012-04-19 11:26:01 +0100

    Use a master key which delegates partial authority to a subkey.
    --HG--
    branch : demo

--- src/test/java/net/lshift/spki/suiteb/demo/TestLoop.java
+++ src/test/java/net/lshift/spki/suiteb/demo/TestLoop.java
@@ -1,17 +1,21 @@
 package net.lshift.spki.suiteb.demo;
 
+import static net.lshift.spki.convert.openable.OpenableUtils.write;
 import static net.lshift.spki.suiteb.Cert.cert;
 import static net.lshift.spki.suiteb.SequenceUtils.sequence;
+import static net.lshift.spki.suiteb.Signed.signed;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Date;
 
 import net.lshift.spki.InvalidInputException;
 import net.lshift.spki.PrettyPrinter;
 import net.lshift.spki.convert.openable.ByteOpenable;
-import net.lshift.spki.convert.openable.OpenableUtils;
+import net.lshift.spki.suiteb.Cert;
+import net.lshift.spki.suiteb.InvalidOnOrAfter;
 import net.lshift.spki.suiteb.PrivateEncryptionKey;
 import net.lshift.spki.suiteb.PrivateSigningKey;
 import net.lshift.spki.suiteb.PublicSigningKey;
@@ -20,21 +24,34 @@ import net.lshift.spki.suiteb.SequenceItem;
 import org.junit.Test;
 
 public class TestLoop {
+    private ByteOpenable writeSequence(SequenceItem... items) throws IOException {
+        ByteOpenable res = new ByteOpenable();
+        write(SequenceItem.class, sequence(items), res);
+        return res;
+    }
+
     @Test
     public void test() throws IOException, InvalidInputException {
         PrivateEncryptionKey decryptionKey = PrivateEncryptionKey.generate();
-        PrivateSigningKey signingKey = PrivateSigningKey.generate();
-        ByteOpenable acl = new ByteOpenable();
-        PublicSigningKey publicKey = signingKey.getPublicKey();
-        OpenableUtils.write(SequenceItem.class, sequence(
-                decryptionKey,
-                publicKey,
-                cert(publicKey.getKeyId())
-            ), acl);
+        PrivateSigningKey masterKey = PrivateSigningKey.generate();
+        PublicSigningKey publicKey = masterKey.getPublicKey();
+        ByteOpenable acl = writeSequence(
+            decryptionKey,
+            cert(publicKey.getKeyId())
+        );
+
+        PrivateSigningKey subKey = PrivateSigningKey.generate();
+        Cert myCert = cert(subKey.getPublicKey().getKeyId(),
+            new InvalidOnOrAfter(new Date(System.currentTimeMillis() + 1000)));
+        ByteOpenable extra = writeSequence(
+            publicKey,
+            subKey.getPublicKey(),
+            masterKey.sign(myCert),
+            signed(myCert));
 
         Service service = new Service("http", 80);
         ByteOpenable target = new ByteOpenable();
-        WriteService.writeService(signingKey, decryptionKey.getPublicKey(),
+        WriteService.writeService(subKey, extra, decryptionKey.getPublicKey(),
             target, service);
         Service readBack = ReadService.readService(acl, target);
         assertThat(readBack.name, is(service.name));
--- src/test/java/net/lshift/spki/suiteb/demo/WriteService.java
+++ src/test/java/net/lshift/spki/suiteb/demo/WriteService.java
@@ -1,10 +1,13 @@
 package net.lshift.spki.suiteb.demo;
 
+import static net.lshift.spki.convert.openable.OpenableUtils.read;
 import static net.lshift.spki.suiteb.SequenceUtils.sequence;
 import static net.lshift.spki.suiteb.Signed.signed;
+import static net.lshift.spki.suiteb.sexpstructs.EcdhItem.ecdhItem;
 
 import java.io.IOException;
 
+import net.lshift.spki.InvalidInputException;
 import net.lshift.spki.convert.Registry;
 import net.lshift.spki.convert.openable.Openable;
 import net.lshift.spki.convert.openable.OpenableUtils;
@@ -13,23 +16,24 @@ import net.lshift.spki.suiteb.PrivateEncryptionKey;
 import net.lshift.spki.suiteb.PrivateSigningKey;
 import net.lshift.spki.suiteb.PublicEncryptionKey;
 import net.lshift.spki.suiteb.SequenceItem;
-import net.lshift.spki.suiteb.sexpstructs.EcdhItem;
 
 public class WriteService {
     public static void writeService(
         PrivateSigningKey signingKey,
+        Openable extra,
         PublicEncryptionKey recipient,
         Openable target,
         Service service)
-                    throws IOException {
+                    throws IOException, InvalidInputException {
         Registry.getConverter(Service.class);
         PrivateEncryptionKey ephemeral = PrivateEncryptionKey.generate();
         final Action action = new Action(service);
         OpenableUtils.write(SequenceItem.class,
             sequence(
                 ephemeral.getPublicKey(),
-                EcdhItem.ecdhItem(ephemeral, recipient),
+                ecdhItem(ephemeral, recipient),
                 ephemeral.getKeyAsSender(recipient).encrypt(sequence(
+                    read(SequenceItem.class, extra),
                     signingKey.sign(action),
                     signed(action)))),
         target);
