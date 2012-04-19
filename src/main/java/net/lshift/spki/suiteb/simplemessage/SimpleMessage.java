a0d7d4bd255d056183dded67e16a46a5ec1330ab
Paul Crowley <paul@lshift.net>
2012-04-19 09:44:58 +0100

    Remove unused imports
    --HG--
    branch : delegation

--- src/main/java/net/lshift/spki/suiteb/simplemessage/SimpleMessage.java
+++ src/main/java/net/lshift/spki/suiteb/simplemessage/SimpleMessage.java
@@ -3,9 +3,6 @@ package net.lshift.spki.suiteb.simplemessage;
 import net.lshift.spki.convert.Convert;
 import net.lshift.spki.suiteb.ActionType;
 
-import org.apache.commons.lang.builder.EqualsBuilder;
-import org.apache.commons.lang.builder.HashCodeBuilder;
-
 /**
  * Format for a simple kind of message - just identifier and data - that
  * can be encrypted and/or signed.
