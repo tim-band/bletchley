42008db76a95eb4d6e4b1026aaa53e26160f6b95
David Ireland <david@lshift.net>
2013-06-13 09:57:39 +0100

    Canonical UUIDs

---
+++ src/test/java/net/lshift/spki/convert/UUIDConverterTest.java
@@ -0,0 +1,42 @@
+package net.lshift.spki.convert;
+
+import static net.lshift.spki.convert.UUIDConverter.validUUID;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.StringTokenizer;
+import java.util.UUID;
+
+import org.junit.Test;
+
+public class UUIDConverterTest
+{
+    @Test
+    public void testValidateCorrect() {
+        assertTrue(validUUID(UUID.randomUUID().toString()));
+    }
+
+    @Test
+    public void testValidateLongSegments() {
+        testPartFormat("%012x");
+        testPartFormat("%08x");
+    }
+
+    private void testPartFormat(String partFormat) {
+        StringBuffer buffer = new StringBuffer();
+        StringTokenizer tokens = new StringTokenizer(UUID.randomUUID().toString(), "-");
+        buffer.append(String.format(partFormat, Long.decode("0x" + tokens.nextToken())));
+        buffer.append('-');
+        buffer.append(String.format(partFormat, Long.decode("0x" + tokens.nextToken())));
+        buffer.append('-');
+        buffer.append(String.format(partFormat, Long.decode("0x" + tokens.nextToken())));
+        buffer.append('-');
+        buffer.append(String.format(partFormat, Long.decode("0x" + tokens.nextToken())));
+        buffer.append('-');
+        buffer.append(String.format(partFormat, Long.decode("0x" + tokens.nextToken())));
+        // Just prove that UUID#fromString will accept the result
+        UUID.fromString(buffer.toString());
+        // And that we don't
+        assertFalse(buffer.toString(), validUUID(buffer.toString()));
+    }
+}
