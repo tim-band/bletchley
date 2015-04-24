79f2d60f6de58d5e1b79139ae3f7032e0e1360b5
jarek <jarek@lshift.net>
2015-04-24 10:56:45 +0100

    Adding meaning messages to invalig field exceptions

--- src/main/java/net/lshift/spki/convert/NameBeanConverterFactory.java
+++ src/main/java/net/lshift/spki/convert/NameBeanConverterFactory.java
@@ -45,7 +45,7 @@ implements ConverterFactory<Convert.ByName>
         for (final Field f: declaredFields) {
             final String fname = f.getName();
             if (!ConvertUtils.isAsciiIdentifier(fname)) {
-                throw new IllegalArgumentException();
+                throw new IllegalArgumentException("Field name is non ascii: " + fname);
             }
             if (!fname.startsWith("$") &&
                             (f.getModifiers() & Modifier.STATIC) == 0)
--- src/main/java/net/lshift/spki/convert/PositionBeanConverterFactory.java
+++ src/main/java/net/lshift/spki/convert/PositionBeanConverterFactory.java
@@ -22,7 +22,7 @@ implements ConverterFactory<Convert.ByPosition>
         final List<FieldConvertInfo> fields = new ArrayList<FieldConvertInfo>();
         for (final String fname: fieldNames) {
             if (!ConvertUtils.isAsciiIdentifier(fname)) {
-                throw new IllegalArgumentException();
+                throw new IllegalArgumentException("Field name is non ascii: " + fname);
             }
             fields.add(new FieldConvertInfo(clazz, getField(clazz, fname)));
         }
@@ -39,7 +39,7 @@ implements ConverterFactory<Convert.ByPosition>
         final String fname) {
         try {
             if (!ConvertUtils.isAsciiIdentifier(fname)) {
-                throw new IllegalArgumentException();
+                throw new IllegalArgumentException("Field name is non ascii: " + fname);
             }
             return c.getDeclaredField(fname);
         } catch (final NoSuchFieldException e) {
