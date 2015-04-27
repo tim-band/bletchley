e4205eff0ffbc92229580c87fb0527e2fcedb9b5
jarek <jsiembida@gmail.com>
2015-04-28 00:18:22 +0100

    Moving the remaining DateFormat related code DateConverter, removing Constants.java

--- src/main/java/net/lshift/spki/convert/DateConverter.java
+++ src/main/java/net/lshift/spki/convert/DateConverter.java
@@ -1,25 +1,41 @@
 package net.lshift.spki.convert;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Date;
-
-import net.lshift.spki.Constants;
+import java.util.TimeZone;
 
 /**
  * Convert between a Date and a SExp
  */
 public class DateConverter
-    extends StringStepConverter<Date> {
-    public DateConverter() { super(Date.class); }
+        extends StringStepConverter<Date> {
+
+    private static final DateFormat DATE_FORMAT;
+
+    static {
+        DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss");
+        DATE_FORMAT.setTimeZone(TimeZone.getTimeZone("UTC"));
+    }
+
+    public static DateFormat getDateFormat() {
+        // Astonishingly, DateFormat isn't thread safe.
+        return (DateFormat) DATE_FORMAT.clone();
+    }
+
+    public DateConverter() {
+        super(Date.class);
+    }
 
     @Override
     protected String stepIn(final Date o) {
-        return Constants.getDateFormat().format(o);
+        return getDateFormat().format(o);
     }
 
     @Override
     protected Date stepOut(final String o) throws ConvertException {
         try {
-            return Constants.getDateFormat().parse(o);
+            return getDateFormat().parse(o);
         } catch (final java.text.ParseException e) {
             throw new ConvertException(e);
         }
