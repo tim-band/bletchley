5b44fe35dd288192c3100fc5a179435ad27c4365
David Ireland <david.ireland@oliverwyman.com>
2019-05-09 09:02:35 +0100

    re BLET-47 restructure this to suite user authenticated keys

--- src/main/java/net/lshift/spki/suiteb/Jca.java
+++ src/main/java/net/lshift/spki/suiteb/Jca.java
@@ -1,6 +1,7 @@
 package net.lshift.spki.suiteb;
 
 import java.io.IOException;
+import java.io.OutputStream;
 import java.math.BigInteger;
 import java.security.InvalidKeyException;
 import java.security.KeyPair;
@@ -16,16 +17,17 @@ import net.lshift.spki.suiteb.sexpstructs.ECPointConverter;
 import net.lshift.spki.suiteb.sexpstructs.EcdsaSignature;
 
 /**
- * Import Bletchley objects from other cryptography frameworks.
- * This is mostly motivated by a need to import with Android Keystore
- * @author david
- *
+ * Import Bletchley objects from JCA.
+ * Unlike Bletchley in general, a JCA provider must be configured to
+ * use this module.
+ * This is mostly motivated by a need to use signatures generated
+ * by keys protected by Androids TEE, although it has other uses.
  */
 public class Jca {
     private Jca() {
         // This class cannot be instantiated
     }
-    
+
     /**
      * Convert a JCA ECPublicKey to a Bletchley public signing key,
      * only if the point is valid on Bletchley's EC curve.
@@ -39,12 +41,12 @@ public class Jca {
      * @throws CryptographyException if the point we obtain from the key
      * is not on Bletchley's EC curve.
      */
-    public static PublicSigningKey importPublicSigningKey(ECPublicKey key) 
+    public static PublicSigningKey importPublicSigningKey(ECPublicKey key)
     throws CryptographyException {
         // Note this relies on ECPointConverter.convert rejecting invalid points,
         // rather than on checking that the curve in key is the right curve
-        // I guess there might be points that belong to multiple curves and then 
-        // the conversion will work, but signatures won't validate, but the chance 
+        // I guess there might be points that belong to multiple curves and then
+        // the conversion will work, but signatures won't validate, but the chance
         // of such a point being generated in testing is very small
         return new PublicSigningKey(Ec.toECPublicKeyParameters(
                 ECPointConverter.convert(key.getW().getAffineX(), key.getW().getAffineY())));
@@ -57,64 +59,115 @@ public class Jca {
             throw new IllegalArgumentException("publicKey must be an instance of ECPublicKey");
         }
     }
-    
+
     /**
-     * Convert an ASN.1. DER encoded signature to a Bletchley Signature. Bletchley
-     * Signature includes enough meta data to make it a useful stanza in a message
-     * the key used to make the signature, and the digest which is signed. This is quite
-     * unnatural if you use {@link java.security#Signature} doesn't even
-     * provide a way to sign a digest. The signature must be SHA384withECDSA
-     * @param key used in the signing request
-     * @param digest the digest which was signed
-     * @param signature the bytes of the signature, DER-encoded ASN.1
+     * Convert an ASN.1. DER encoded signature to a Bletchley Signature.
+     * @see Signature#Signature(DigestSha384, DigestSha384, EcdsaSignature)
+     * @param key used in the signing request, so we can attach the key id as meta data
+     * @param digest the digest which was signed, so we can attach it as meta data
+     * @param signature the bytes of the signature, DER-encoded ASN.1. The signature must be SHA384withECDSA.
      * @return the signature
      */
-    public static Signature importSignature(
+    private static Signature importSignature(
             DigestSha384 digest,
             PublicSigningKey key,
             byte [] signature) {
         try {
-            BigInteger [] values = StandardDSAEncoding.INSTANCE.decode(Ec.DOMAIN_PARAMETERS.getN(), signature);
+            BigInteger [] values = StandardDSAEncoding.INSTANCE.decode(
+                    Ec.DOMAIN_PARAMETERS.getN(), signature);
             return new Signature(
-                    digest, 
-                    key.getKeyId(), 
+                    digest,
+                    key.getKeyId(),
                     new EcdsaSignature(values[0], values[1]));
         } catch (IOException e) {
             throw new AssertionError("There's no IO happenning here", e);
         }
     }
 
+    private static class SignatureIOException extends IOException {
+        private static final long serialVersionUID = 1L;
+        public SignatureIOException(SignatureException e) {
+            super(e);
+        }
+    }
+
     /**
-     * This is a higher level interface for signing using JCA. The point of offering
-     * this is so that you can use the fingerprint reader in and Android phone to
-     * sign something. I.e. Use a key pair stored in the TEE. You might also want to
-     * sign things using a PKCS#11 provider.
-     * This method is quite inefficient: it serialises the payload and digests it
-     * twice, because JCA won't just encrypt a digest: you have to sign the actual
-     * data.
+     * This class represents a signing session. See {@link #signer(KeyPair,SequenceItem}
+     */
+    public static class Signer {
+        public final PublicSigningKey publicKey;
+        public final SequenceItem payload;
+        public final java.security.Signature signature;
+
+        private Signer(KeyPair keyPair, SequenceItem payload) throws CryptographyException {
+            this.publicKey = importPublicSigningKey(keyPair.getPublic());
+            this.payload = payload;
+            try {
+                signature = java.security.Signature.getInstance("SHA384withECDSA");
+                signature.initSign(keyPair.getPrivate());
+                ConvertUtils.write(payload, new OutputStream() {
+                    @Override public void write(int b) throws IOException {
+                        try {
+                            signature.update((byte)b);
+                        } catch (SignatureException e) {
+                            throw new SignatureIOException(e);
+                        }
+                    }
+
+                    @Override public void write(byte[] data, int off, int len) throws IOException {
+                        try {
+                            signature.update(data, off, len);
+                        } catch (SignatureException e) {
+                            throw new SignatureIOException(e);
+                        }
+                    }
+
+                });
+            } catch (InvalidKeyException | NoSuchAlgorithmException e) {
+                throw new CryptographyException("JCA provider not compatible", e);
+            } catch(SignatureIOException e) {
+                throw new CryptographyException("JCA exception during signing",e.getCause());
+            } catch (IOException e) {
+                throw new AssertionError(e);
+            }
+        }
+
+        public SequenceItem signed() throws CryptographyException {
+            try {
+                return SequenceUtils.sequence(
+                        importSignature(
+                                DigestSha384.digest(this.payload),
+                                this.publicKey,
+                                signature.sign()),
+                        Signed.signed(this.payload));
+            } catch (SignatureException e) {
+                throw new CryptographyException("JCA exception during signing", e);
+            }
+        }
+    }
+
+    /**
+     * Create a signed sequence, where key use requires authentication.
+     * The signed sequence creates the forward chain that assigns the trust of the key to the payload.
+     * This can, for example, be used with Android's JCA environment for keys
+     * which are stored in the TEE with userAuthenticationRequired set to true. You might
+     * also want to a key held by a PKCS#11 provider.
      * @param keyPair the JCA key pair used in signing
      * @param payload the payload to sign
      * @return a sequence containing the signature and signed payload
-     * @throws CryptographyException
+     * @throws CryptographyException for a range of issues, which are generally that the JCA
+     *   provider doesn't support the primitives provided by Bletchley.
+     */
+    public static Signer signer(KeyPair keyPair, SequenceItem payload)
+        throws CryptographyException {
+        return new Signer(keyPair, payload);
+    }
+
+    /**
+     * Equivalent to {@link #signer(KeyPair, SequenceItem)} and {@link Signer#signed()}.
      */
     public static SequenceItem signed(KeyPair keyPair, SequenceItem payload)
         throws CryptographyException {
-        try {
-            java.security.Signature signature = java.security.Signature.getInstance("SHA384withECDSA");
-            signature.initSign(keyPair.getPrivate());
-            // This serialises the payload and generates the digest twice: once here in signature.update
-            // and then again in DigestSha384.digest(payload) below. I can do slighly better than
-            // this but can't figure out a way to avoid it completely, so I'm just going to ignore
-            // it for now.
-            signature.update(ConvertUtils.toBytes(payload));
-            return SequenceUtils.sequence(
-                    importSignature(
-                            DigestSha384.digest(payload), 
-                            importPublicSigningKey(keyPair.getPublic()),
-                            signature.sign()), 
-                    Signed.signed(payload));
-        } catch (InvalidKeyException | NoSuchAlgorithmException | SignatureException e) {
-            throw new CryptographyException("JCA exception during signing", e);
-        }
+        return signer(keyPair, payload).signed();
     }
 }
